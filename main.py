cadbab48ffcbb12446facfcda1f7ec2bcf4101d2
merricat <zkerr4032@outlook.com>
2024-08-04 15:05:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from typing import NewType
+import curses
+import random
+
+# ComparisonCheck is a comparison of two Word types
+# each integer corrisponds to the letter index of the words that are being copared
+# if the letters don't compare then the integer should be 0
+# if they do compare then the integer will be 2
+# if they don't compare but the guessed letter is somewhere in the word it should be a 1
+ComparisonCheck = NewType('ComparisonCheck', tuple[int, int, int, int, int])
+Word = NewType('Word', tuple[str, str, str, str, str])
+
+def main():
+    word = get_word()
+    print(word)
+    print(compare_words(
+        str_to_word("dafty"),
+        str_to_word("dylpy"),
+    ))
+
+def get_word() -> Word:
+    random_number = random.randrange(0, 14954)
+    word = ""
+    with open("./wordlist", "r") as wl:
+        word = list(wl)[random_number].strip()
+    return str_to_word(word)
+
+def str_to_word(x: str) -> Word:
+    return Word((x[0], x[1], x[2], x[3], x[4]))
+
+def compare_words(word: Word, user_guess: Word) -> ComparisonCheck:
+    checker = [0, 0, 0, 0, 0]    
+    for i in range(0, 5):
+        if user_guess[i] == word[i]:
+            checker[i] = 2
+        elif user_guess[i] in word:
+            checker[i] = 1
+        else:
+            checker[i] = 0
+    return ComparisonCheck((checker[0],
+                            checker[1],
+                            checker[2],
+                            checker[3],
+                            checker[4]))
+
+
+if __name__ == "__main__":
+    main()
